eef7016b605f6ccdb0d3ef886228c2e0e53724df
edu_mb <eduardowemb@gmail.com>
2024-01-18 18:21:56 -0300

    Desabilitando CORS no perfil de Dev.

--- src/main/java/com/saboresdigitais/quickeats/store/infrastructure/config/MvcConfig.java
+++ src/main/java/com/saboresdigitais/quickeats/store/infrastructure/config/MvcConfig.java
@@ -1,17 +1,11 @@
 package com.saboresdigitais.quickeats.store.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+    
 }
--- src/main/java/com/saboresdigitais/quickeats/store/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/saboresdigitais/quickeats/store/infrastructure/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.saboresdigitais.quickeats.store.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -16,28 +19,30 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Profile("dev")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite CORS em todos os caminhos
+                        .allowedOrigins("http://localhost:4200") // Substitua pelo seu front-end origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Profile("dev")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf.disable()) // Desativa a proteção CSRF, não recomendado para produção
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/api/customers").permitAll()
-                .requestMatchers("/api/customers/*").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll() // Permite todas as requisições sem autenticação
             )
-            .formLogin((form) -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login-error")
-                .permitAll()
-            )
-            .logout((logout) -> logout
-                .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-            )
-            .exceptionHandling((ex) -> ex
-                .accessDeniedPage("/negado")
-            );
+            .httpBasic(httpBasic -> httpBasic.disable()); // Desativa a autenticação básica HTTP
+
         return http.build();
     }
 }
